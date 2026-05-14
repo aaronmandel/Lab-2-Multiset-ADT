7971c4c68c7e9b9c3b2c9703ad3fe5be1b265cd9
aaronmandel <aforaaron@gmail.com>
2026-05-14 19:55:13 -0400

    test1

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -9,6 +9,7 @@ public abstract class MultiSet {
      * @param item the item to add
      */
     abstract void add(Integer item);
+    abstract void sadsak();
+    abstract boolean testingthing();
 
-    // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
 }
